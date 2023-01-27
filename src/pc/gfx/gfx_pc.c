4c1de0ff452775cf673162b1a10e7bf2c3ae68dc
Raymond <rayzerbrain@gmail.com>
2023-01-27 17:49:24 -0600

    macro readability

--- src/pc/gfx/gfx_pc.c
+++ src/pc/gfx/gfx_pc.c
@@ -68,15 +68,15 @@ typedef float sfrac32;
 #else
 typedef int32_t sfrac32;
 
-#define FIX_2_INT(fix) ((fix) >> 16) // non-rounding
-#define FIX_2_FLOAT(fix) ((fix) / 65536.f) // 2^16
-#define INT_2_FIX(num) ((num) << 16)
-#define FLOAT_2_FIX(fl) ((int32_t)((fl) * 65536)) //2^16
+#define FIX_2_INT(fix)  ( (fix) >> 16 ) // non-rounding
+#define FIX_2_FLOAT(fix)  ( (fix) / 65536.f ) // 2^16
+#define INT_2_FIX(num)  ( (num) << 16 )
+#define FLOAT_2_FIX(fl)  ( (int32_t)((fl) * 65536) ) //2^16
 
 
-#define FIX_ADD_INT(fix, num) ( (fix) + (num) >> (sizeof(sfrac32) * 4) )
-#define FIX_MUL(fix1, fix2) ( (int32_t)(((int64_t)(fix1) * (fix2)) >> 16) )
-#define FIX_MUL_FLOAT(fix, fl) ( (int32_t)((int64_t) (fix) * (fl)) )
+#define FIX_ADD_INT(fix, num)  ( (fix) + (num) >> 16 )
+#define FIX_MUL(fix1, fix2)  ( (int32_t) (((int64_t)(fix1) * (fix2)) >> 16) )
+#define FIX_MUL_FLOAT(fix, fl)  ( (int32_t)((fix) * (fl)) )
 #endif
 
 enum {
